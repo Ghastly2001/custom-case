da2b5d8e3a1a503b91a641e189e383bff8a562d0
Ghastly2001 <divyanshverma18may@gmail.com>
2024-05-20 14:19:24 +0530

    schema changes

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { z } from "zod";
 import sharp from "sharp";
+import { db } from "@/db";
 
 const f = createUploadthing();
 export const ourFileRouter = {
@@ -19,7 +20,24 @@ export const ourFileRouter = {
       const { width, height } = imageMetadata;
 
       //check if config id is passed or not
-      return { configId };
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        });
+        return { configId: configuration.id };
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: { id: configId },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        });
+        return { configId: updatedConfiguration.id };
+      }
     }),
 } satisfies FileRouter;
 
